546736fe928b9172a0056227b5bc2e369d59db64
Raeaw <raffi.darrell.f@gmail.com>
2026-04-27 14:17:46 +0700

    feat: implement google oauth2 authorization code flow

--- services/auth-service/routes/authRoutes.js
+++ services/auth-service/routes/authRoutes.js
@@ -3,10 +3,13 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 
+// Endpoint GET
+router.get('/google', authController.googleLogin);
+router.get('/google/callback', authController.googleCallback);
+
 // Endpoint POST
 router.post('/register', authController.register);
 router.post('/login', authController.login);
-
 router.post('/refresh', authController.refreshToken);
 router.post('/logout', authController.logout);
 
--- services/auth-service/server.js
+++ services/auth-service/server.js
@@ -1,7 +1,7 @@
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
 const app = express();
 
@@ -9,23 +9,24 @@ const app = express();
 app.use(cors());
 app.use(express.json()); // Agar bisa menerima payload JSON
 
-const authRoutes = require('./routes/authRoutes');
+const authRoutes = require("./routes/authRoutes");
 
 // Cek koneksi ke MongoDB
-mongoose.connect(process.env.MONGO_URI)
-    .then(() => console.log('Terhubung ke Database MongoDB (Auth Service)'))
-    .catch((err) => console.error('Gagal terhubung ke MongoDB:', err.message));
+mongoose
+	.connect(process.env.MONGO_URL)
+	.then(() => console.log("Terhubung ke Database MongoDB (Auth Service)"))
+	.catch((err) => console.error("Gagal terhubung ke MongoDB:", err.message));
 
 // Rute tes dasar
-app.get('/api/auth/health', (req, res) => {
-    res.status(200).json({ status: 'OK', message: 'Auth Service is running' });
+app.get("/api/auth/health", (req, res) => {
+	res.status(200).json({ status: "OK", message: "Auth Service is running" });
 });
 
 // Routes
-app.use('/api/auth', authRoutes);
+app.use("/api/auth", authRoutes);
 
 // Menjalankan server
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-    console.log(`Auth Service berjalan di http://localhost:${PORT}`);
+	console.log(`Auth Service berjalan di http://localhost:${PORT}`);
 });
